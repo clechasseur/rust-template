59642b32da8da4c4003bc72757e0578f0e012dad
Charles Lechasseur <shiftingbeard@gmx.com>
2023-10-21 18:42:05 -0400

    chore: enable workflows

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,19 @@ pub fn hello() -> &'static str {
     "Hello, World!"
 }
 
+/// Returns a string for feature!
+/// 
+/// ```
+/// use rust_template_output::for_feature;
+/// 
+/// let foo = for_feature();
+/// assert!(!foo.is_empty());
+/// ```
+#[cfg(feature = "test_feature_1")]
+pub fn for_feature() -> &'static str {
+    "For feature!"
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +31,16 @@ mod tests {
     fn test_lib_one() {
         let foo = hello();
         assert!(!foo.is_empty());
+    }
+
+    #[cfg(feature = "test_feature_1")]
+    mod for_feature {
+        use super::*;
+
+        #[test]
+        fn test_for_feature_one() {
+            let foo = for_feature();
+            assert!(!foo.is_empty());
+        }
     }
 }
